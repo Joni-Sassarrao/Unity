30e98796152afd889df73a3e04ee109cea9c817a
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-03 11:30:59 -0300

    FlappyBird

--- FlappyBird/Flappy Bird/Assets/Scripts/Obstaculo.cs
+++ FlappyBird/Flappy Bird/Assets/Scripts/Obstaculo.cs
@@ -5,20 +5,30 @@ using UnityEngine;
 public class obstaculo : MonoBehaviour
 {
     [SerializeField]
-    private float velocidade = 0.6f;
+    private float velocidade = 1;
 
     [SerializeField]
     private float variacaoposicaoY;
     void Start()
     {
-        
+
     }
     private void Awake()
     {
-        this.transform.Translate(Vector3.up * Random.Range(-1,1));  
+        this.transform.Translate(Vector3.up * Random.Range(-variacaoposicaoY, variacaoposicaoY));
     }
     void Update()
     {
         this.transform.Translate(Vector3.left * velocidade * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        this.Destruir();
+    }
+
+    private void Destruir()
+    {
+        Destroy(this.gameObject);
     }
 }
